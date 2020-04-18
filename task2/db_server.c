b599a18a33b63d149a0ce505cfd07cd8a5269c77
pavlo <pavel.skirko@gmail.com>
2020-04-19 00:34:50 +0300

    task2: init

---
+++ task2/db_server.c
@@ -0,0 +1,6 @@
+#include "db_server.h"
+
+int main()
+{
+    printf("here we go");
+}
---
+++ task2/include/db_server.h
@@ -0,0 +1,8 @@
+#ifndef DB_SERVER
+#define DB_SERVER
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#endif
